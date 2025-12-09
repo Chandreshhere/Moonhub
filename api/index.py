e11fb7be1b94a30d83bee142a036175db7003958
Chandresh <whoischandresh@gmail.com>
2025-12-09 18:44:11 +0530

    Initial commit: MoonHub Inventory Management System - Vercel ready

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 import sys
+import os
 from pathlib import Path
 
 # Add parent directory to path
-sys.path.insert(0, str(Path(__file__).parent.parent))
+parent_dir = str(Path(__file__).parent.parent)
+sys.path.insert(0, parent_dir)
+
+# Set working directory
+os.chdir(parent_dir)
 
 from web_dashboard import app
 
-# Export app for Vercel
+# Vercel serverless function handler
 app = app
